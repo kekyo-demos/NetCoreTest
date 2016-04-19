d26710599148fc69465eebe4259ae2675e880dad
Kouji Matsui <k@kekyo.net>
2016-04-19 18:46:19 +0900

    Add StoreApp / WP test codes.

--- Net461UnitTest/UnitTest1.cs
+++ Net461UnitTest/UnitTest1.cs
@@ -1,24 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.Linq;
 using System.Text;
-using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+using System.Threading.Tasks;
+using NUnit.Framework;
 
-namespace Win8UnitTest
+namespace Net461UnitTest
 {
-    [TestClass]
+    [TestFixture]
     public class UnitTest1
     {
-        [TestMethod]
-        public void NetCoreTest()
+        [Test]
+        public void Net461Test()
         {
             var type = WithEmit.Emitter.CreateTestType();
             var mi = type.GetDeclaredMethod("TestMethod");
             //var pis = mi.GetParameters();
             //var d = (Func<int, double, string>)mi.CreateDelegate(typeof(Func<int, double, string>), mi);
             //var result = d(123, 456.789);
-            var result = (string)mi.Invoke(null, new object[] { 123, 456.789 });
+            var result = (string)mi.Invoke(null, new object[] {123, 456.789});
 
             Assert.AreEqual("123-456.789", result);
         }
---
+++ WithEmit/Emitter.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using System.Reflection.Emit;
+
+namespace WithEmit
+{
+    public static class Emitter
+    {
+        //public static void Main(string[] args)
+        //{
+        //    //        Console.WriteLine("Hello World!");
+
+        //    //     try
+        //    //     {
+        //    //Console.WriteLine( "Success: {0}", TryCreateEmptyType() );
+        //    //     }
+        //    //     catch ( Exception ex )
+        //    //     {
+        //    //Console.WriteLine( "Failed :{0}", ex );
+        //    //     }
+
+        //    TryCreateEmptyType();
+        //}
+
+        //private static TypeInfo TryCreateEmptyType()
+        //{
+        //    return CreateEmptyType( "FromConsole" );
+        //}
+
+
+        public static TypeInfo CreateTestType()
+        {
+            //  public static System.Reflection.Emit.AssemblyBuilder DefineDynamicAssembly(System.Reflection.AssemblyName name, System.Reflection.Emit.AssemblyBuilderAccess access) { return default(System.Reflection.Emit.AssemblyBuilder); }
+            var asm = AssemblyBuilder.DefineDynamicAssembly( new AssemblyName( "HostAssembly" ), AssemblyBuilderAccess.RunAndCollect );
+            var mod = asm.DefineDynamicModule( "HostAssembly.dll" );
+            var type = mod.DefineType("WithEmit_TestType", TypeAttributes.Public | TypeAttributes.Class);
+            var method = type.DefineMethod(
+                "TestMethod",
+                MethodAttributes.Public | MethodAttributes.Static,
+                CallingConventions.Standard,
+                typeof(string),
+                new[] { typeof(int), typeof(double) });
+            var ilg = method.GetILGenerator();
+            ilg.Emit(OpCodes.Ldstr, "{0}-{1}");
+            ilg.Emit(OpCodes.Ldarg_0);
+            ilg.Emit(OpCodes.Box, typeof(int));
+            ilg.Emit(OpCodes.Ldarg_1);
+            ilg.Emit(OpCodes.Box, typeof(double));
+
+            var stringType = typeof(string).GetTypeInfo();
+            var parameterTypes = new[] {typeof(string), typeof(object), typeof(object)};
+            var formatMethod = stringType.DeclaredMethods.First(mi =>
+                (mi.Name == "Format") &&
+                (mi.GetParameters().Select(p => p.ParameterType).SequenceEqual(parameterTypes)));
+
+            ilg.Emit(OpCodes.Call, formatMethod);
+            ilg.Emit(OpCodes.Ret);
+
+            return type.CreateTypeInfo();
+        }
+    }
+}
